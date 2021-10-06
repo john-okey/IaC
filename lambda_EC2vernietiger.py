bb1acb7eda74fa9bdc20d9d01afc86e6deb5f93b
johnokey <mr.john.okey@gmail.com>
2021-10-06 22:33:50 +1100

    Update lambda_EC2vernietiger.py

--- lambda_EC2vernietiger.py
+++ lambda_EC2vernietiger.py
@@ -1,8 +1,8 @@
 # bron: https://aws.amazon.com/premiumsupport/knowledge-center/start-stop-lambda-cloudwatch/
 #
 import boto3
-region = 'ap-southeast-2'
-instances = ['i-0c3eef1874b338d56', 'i-02c43780b03a9c55a']
+region = '<region_x>'
+instances = ['i-<instance_id1>', 'i-<instance_id2>']
 ec2 = boto3.client('ec2', region_name=region)
 
 def lambda_handler(event, context):
